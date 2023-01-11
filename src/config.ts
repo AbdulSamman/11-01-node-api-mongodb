731a5f4cd5bdfcc3945b732d0464fb6c90e3b580
Your Name <you@example.com>
2023-01-11 11:38:42 +0100

    pormise added

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,6 @@
-export const port = 3610;
-export const logPathAndFileName = './src/logs/log.txt';
+import dotenv from "dotenv";
+dotenv.config();
+
+export const port = process.env.PORT || 3000;
+export const logPathAndFileName = "./src/logs/log.txt";
+export const mongoDbConnection = "mongodb://localhost:27017";
--- src/model.ts
+++ src/model.ts
@@ -1,20 +1,43 @@
-import fs from 'fs';
-import { IBook, IRawBook } from './interfaces.js';
+import * as config from "./config.js";
+import { MongoClient } from "mongodb";
+import * as model from "./model.js";
+const client = new MongoClient(config.mongoDbConnection);
 
-const rawBooks: IRawBook[] = JSON.parse(fs.readFileSync('./src/data/rawBooks.json', 'utf8'));
+const accessDatabase = async (done: (db: any) => void) => {
+  await client.connect();
+  const db = client.db(`northwind`);
+  done(db);
+};
 
-const books: IBook[] = rawBooks.map(rawBook => {
-	return {
-		id: rawBook.id,
-		idCode: rawBook.idCode,
-		title: rawBook.title,
-		description: rawBook.description,
-		language: rawBook.language === '' ? 'english' : rawBook.language
-	}
-});
+export const getEmployees = (): any => {
+  // wir brauchen promise da employees nicht als return angeben können, da async await langsamer ist, => brauchen return nicht mehr und await in server
+  return new Promise((resolve, reject) => {
+    try {
+      accessDatabase(async (db) => {
+        // employees rausholen
+        const employees = await db
+          .collection("employees")
+          .find({})
+          .project({ firstName: 1, lastName: 1, _id: 0 }) // our select
+          .toArray();
+        if (employees.length > 0) {
+          resolve(employees);
+        } else {
+          reject({
+            status: "error",
+            message: "bad collection name",
+          });
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+    // return employees;
+  });
+};
 
 export const getApiInstructions = () => {
-	return `
+  return `
 <style>
 	body {
 		background-color: #444;
@@ -32,12 +55,4 @@ export const getApiInstructions = () => {
 	<li><code>/books/3</code> - book with id 3</li>
 </ul>
 	`;
-}
-
-export const getBooks = (): IBook[] => {
-	return books;
-}
-
-export const getBook = (id: number): IBook => {
-	return books.find(m => m.id === id);
-}
+};
